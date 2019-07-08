b07e29c4950220809a5033ca9987d76c29fca054
Yariv Shamash <yariv.shamash@gmail.com>
2019-07-08 18:29:48 +0300

    async/await refractor of savePlaylist() is now working :)

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -30,7 +30,6 @@ export const Spotify = {
             return userAccessToken;
         }
         
-        /*Using the below definitions with http://localhost:3000 creates problems because window.location.href evaluates to http://localhost:3000/ */
         let userAccessTokenMatch = window.location.href.match(/access_token=([^&]*)/); /*returns an array with the item access_token=<whatever it is set to> untill the '&' sign*/
         let expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);  /*returns an array with the item expires_in=<whatever it is set to> untill the '&' sign*/
         
@@ -115,8 +114,7 @@ export const Spotify = {
       let error;
       const headers = {Authorization: `Bearer ${userAccessToken}`};
       let userID;
-
-      let playlistId;
+      let playlistID;
 
       if (!playlistName || !trackURI.length){
         return Promise.reject(new Error(`${topic} failed. Confirm that the playlist has a name and some songs in it`)).then(()=>{}, error => console.log(error));
@@ -124,39 +122,25 @@ export const Spotify = {
       
       
       
-      /*The code below tries to GET the user's ID */
+      /*The code below tries to GET the user's ID from the spotify API */
 
       try {
         response = await fetch(`${SPOTIFY_SEARCH_ENDPOINT}/me`, {
           method: 'GET',
           headers: headers
         })
+        .then( response => response.json())
+        .then(JSONResponse => userID = JSONResponse.id)
+
       } catch (e) {
         error = new Error(`Network error, while ${topic}.`);
         return Promise.reject(error);
       };
 
-      if (!response.ok) {
-        error = new Error(`Server error, status: ${response.status}, while ${topic}.`);
-        return Promise.reject(error);
-      };
-
-      // JSONifying the response
-      try {
-        JSONResponse = await response.json;
-        
-      } catch (err){
-        error = new Error(`Fail to convert response to JSON format, while ${topic}.`);
-        return Promise.reject(error);
-      }
-
-      try {
-        userID = await JSONResponse.id;
-        console.log(userID); // userID is undefined and causing error status 403.
-      } catch (err){
-        error = new Error(`Fail to save userID, while ${topic}.`);
-        return Promise.reject(error);
-      }
+      // if (!response.ok) {
+      //   error = new Error(`Server error, status: ${response.status}, while ${topic}.`);
+      //   return Promise.reject(error);
+      // }; // not sure the condition above is needed..
 
       
       /*The code below tries to POST a new playlist in the user’s account and returns a playlist ID */
@@ -166,35 +150,18 @@ export const Spotify = {
           method: 'POST',
           body: JSON.stringify({name: playlistName})
           })
+          .then(response => response.json())
+          .then(JSONResponse => playlistID = JSONResponse.id)
           
       } catch (e) {
         error = new Error(`Network error, while posting the PlaylistID during ${topic}.`);
         return Promise.reject(error);
       };
 
-      if (!response.ok) {
-        error = new Error(`Server error, status: ${response.status}, while posting the PlaylistID during ${topic}.`);
-        return Promise.reject(error);
-      };
-
-      // JSONifying the response
-      try {
-        JSONResponse = await response.json;
-      } catch (err){
-        error = new Error(`Fail to convert response to JSON format, while posting the PlaylistID during ${topic}.`);
-        return Promise.reject(error);
-      }
-      try {
-        playlistId = JSONResponse.id;
-      } catch (err){
-        error = new Error(`Fail to save playlistID, while posting the PlaylistID during ${topic}.`);
-        return Promise.reject(error);
-      }
-
 
        /*The code below tries to POST the track URIs to the above playlist*/
       try {
-        response = await fetch(`${SPOTIFY_SEARCH_ENDPOINT}/users/${userID}/playlists/${playlistId}/tracks`, {
+        response = await fetch(`${SPOTIFY_SEARCH_ENDPOINT}/users/${userID}/playlists/${playlistID}/tracks`, {
           headers: headers,
           method: 'POST',
           body: JSON.stringify({uris: trackURI})
